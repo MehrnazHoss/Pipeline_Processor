741aa2cd9cd34d635635f0538bf1fbfca6a98276
MehrnazHoss <bangtans.army.mh.2013@email.com>
2025-06-25 17:05:42 +0330

    Add DataMemory ( Writing to mem on the rising edge of clk using the MemWrite signal and reading from mem using the MemRead signal )

--- DataMemory.v
+++ DataMemory.v
@@ -4,6 +4,19 @@ module DataMemory (
     input  wire        MemWrite,
     input  wire [31:0] address,
     input  wire [31:0] writeData,
-    output wire [31:0] readData
+    output reg [31:0] readData
 );
+    reg [31:0] mem[0:1023]; 
+    
+    always @(posedge clk) begin
+        if (MemWrite) 
+            mem[address[11:2]] <= writeData;
+    end
+    
+    always @(*) begin
+        if (MemRead) 
+            readData <= mem[address[11:2]];
+        else
+            readData <= 32'b0;
+    end
 endmodule
