c53fd1560ae526deb1b1d8af044e93f31a6ed9a8
MehrnazHoss <bangtans.army.mh.2013@email.com>
2025-06-26 20:15:25 +0330

    Add RegisterFile ( Write to the register at non-zero address writeReg, on the positive edge of clk using the RegWrite signal )

--- RegisterFile.v
+++ RegisterFile.v
@@ -8,4 +8,19 @@ module RegisterFile (
     output reg  [31:0] readData1,
     output reg  [31:0] readData2
 );
+
+    reg [31:0] regs[0:31];
+
+    initial regs[0] = 32'b0;
+
+    always @(*) begin
+        readData1 = regs[readReg1];
+        readData2 = regs[readReg2];
+    end
+    
+    always @(posedge clk) begin
+        if (RegWrite && writeReg != 0)
+            regs[writeReg] <= writeData;
+    end
+
 endmodule
